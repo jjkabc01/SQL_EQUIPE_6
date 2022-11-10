d48950409c0c821f60894bfe2184eb464c990f89
jjkabc01 <87664494+jjkabc01@users.noreply.github.com>
2022-11-09 20:32:44 -0400

    1)h)i
    1)h)i ->fait

--- TP2.sql
+++ TP2.sql
@@ -1,17 +1,18 @@
-/************ Question 1)d)  ***/
+  /*1h) Afficher le nom et l’état des notifications créées par l’administrateur ayant pour nom «Thomas» et pour prénom «Paul»  */
    
-   insert into TP2_CONFERENCE ( SIGLE_CONFERENCE, TITRE_CON, DATE_DEBUT_CON, DATE_FIN_CON, LIEU_CON, ADRESSE_CON)
-    values ('CONF_4', 'TITRE_4', to_date('21-08-01','RR-MM-DD'), to_date('21-09-01','RR-MM-DD'), 'ROME', '22 RUE DE LA CATHEDRALE SAINT
-    ');
-    
-    insert into TP2_INSCRIPTION_CONFERENCE ( SIGLE_CONFERENCE, NO_MEMBRE, DATE_DEMANDE_INS, STATUT_APPROBATION_INS)
-    values ('CONF_4', 20,  to_date('21-08-15','RR-MM-DD'), 1);
-    
-    delete
-        from (select *  from TP2_INSCRIPTION_CONFERENCE I, TP2_CONFERENCE C
-          where C.SIGLE_CONFERENCE = I.SIGLE_CONFERENCE 
-          and C.DATE_FIN_CON < (sysdate - 500) 
-          );
-          
-   delete 
-    from TP2_CONFERENCE where DATE_FIN_CON < (sysdate - 500);
+   /*1)h)i) avec un in */
+  
+    insert into TP2_MEMBRE( NO_MEMBRE,  UTILISATEUR_MEM, MOT_DE_PASSE_MEM, NOM_MEM, PRENOM_MEM, ADRESSE_MEM, CODE_POSTAL_MEM, PAYS_MEM, TEL_MEM, FAX_MEM, LANGUE_CORRESPONDANCE_MEM,
+  NOM_FICHIER_PHOTO_MEM, ADRESSE_WEB_MEM, INSTITUTION_MEM, COURRIEL_MEM, NO_MEMBRE_PATRON, EST_ADMINISTRATEUR_MEM, EST_SUPERVISEUR_MEM, EST_APPOUVEE_INSCRIPTION_MEM) 
+    values ( NO_MEMBRE_SEQ.nextval, 'thomas.paul', FCT_GENERER_MOT_DE_PASSE(7), 'Paul', 'Thomas', '10 boulevard Park 4 Sauthoff Circle', 'h3e 4t1', 'USA', '(514)699-1569','(514)199-4569','Anglais','/membre1.png','Jhondoes.com','NASA','thomas.paul@cipre.com', 5 ,1,0,1);
+  
+  
+    insert into TP2_NOTIFICATION ( NO_NOTIFICATION, NOM_NOT, DATE_ECHEANCE_NOT, ETAT_NOT, NOTE_NOT, NO_MEM_ADMIN_CREATION, NO_MEM_ATTRIBUTION) 
+    values ( NO_NOTIFICATION_SEQ.nextval, 'Nom_notif_2_thomas', to_date('15-09-01','RR-MM-DD'), 'Non débutée', 'Note notification_1', 110, 10);
+  
+
+  select NOM_NOT, ETAT_NOT
+  from TP2_NOTIFICATION N, TP2_MEMBRE M
+  where N.NO_MEM_ADMIN_CREATION = M.NO_MEMBRE and NO_MEMBRE in (select NO_MEMBRE
+                                                           from TP2_MEMBRE
+                                                           where NOM_MEM = 'Paul' and PRENOM_MEM = 'Thomas');
